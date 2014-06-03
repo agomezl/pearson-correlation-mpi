1df7155f207aa7fccbc35f48827b608735957bd4
agomezl <alegomez544@gmail.com>
2014-06-02 23:53:58 -0500

    improvements modified:   src/Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -13,41 +13,35 @@ int * matrixSr;
 int users ,movies, curnode, m;
 
 int main(int argc, char *argv[]) {
-
+  //MPI stuff
    char hostname[MPI_MAX_PROCESSOR_NAME];
-
    rc = MPI_Init(&argc,&argv);
    if (rc != MPI_SUCCESS) {
      printf ("Error starting MPI program. Terminating.\n");
      MPI_Abort(MPI_COMM_WORLD, rc);
    }
-
    MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
    MPI_Get_processor_name(hostname, &len);
    printf ("Number of tasks= %d My rank= %d Running on %s\n",
            numtasks,rank,hostname);
-
    // Read the file
-
    freopen(argv[1],"r",stdin);
-
    // Make some space for the matrix
    cin >> users >> movies >> m;
    matrixUI = new int*[users];
    for (int i = 0; i < users; i++) matrixUI[i] = new int[movies];
    if (rank == 0) matrixSr = new int[users*m];
-
    // Fill the matrix whit the dat
    for (int i = 0 ; i < users; i++)
      for (int j = 0; j < movies; j++)
        cin >> matrixUI[i][j];
-
+   //Core check
    if (numtasks > users){
      cout << "too many cores" << endl;
      MPI_Abort(MPI_COMM_WORLD, rc);
    }
-
+   //Split the users among the process
    int slice,rem,minU,maxU;
    slice = users / numtasks;
    minU  = rank*slice;
@@ -55,7 +49,7 @@ int main(int argc, char *argv[]) {
      maxU = users;
      slice = users / numtasks + users % numtasks;
    }else maxU = rank*slice + slice;
-
+   //Computes the sub-matrix assigned for that process
    int vectSR[m*slice];
    double corMatrix [users];
    double best;
@@ -79,22 +73,21 @@ int main(int argc, char *argv[]) {
        corMatrix[bestIndx] = -1e9;
      }
    }
-
+   //Gathers the number of users assigned to each process
    int counts[numtasks],offs[numtasks],off;
-
    MPI_Allgather(&slice,1,MPI_INT,counts ,1,MPI_INT,MPI_COMM_WORLD);
-
+   //Computes offsets
    off = 0;
    for(int j=0;j<numtasks;j++){
      offs[j]=off;
      counts[j] *= m;
      off  += counts[j];
    }
-
+   //Put all the sub-matrices together
    MPI_Gatherv(vectSR,slice*m,MPI_INT,
                matrixSr,counts,offs,MPI_INT,
                0,MPI_COMM_WORLD);
-
+   //Print results
    if (rank == 0)
      for (int i = 0; i < users; i++){
        for (int j = 0 ; j < m; j++)
@@ -105,26 +98,26 @@ int main(int argc, char *argv[]) {
     MPI_Finalize();
 }
 
- double corr(int a, int u) {
-   double pa,pu;
-   pa = pu = 0.0;
-   for (int i = 0; i < movies; i++) {
-     pa += matrixUI[a][i];
-     pu += matrixUI[u][i];
-   }
-   pa /= movies;
-   pu /= movies;
-   double con=0;
-   for (int i = 0; i < movies; ++i){
-     con += (matrixUI[a][i]-pa)*(matrixUI[u][i]-pu);
-   }
-   double cod1 = 0;
-   double cod2 = 0;
-   for (int i = 0; i < movies; ++i) {
-     cod1 += (matrixUI[a][i]-pa)*(matrixUI[a][i]-pa);
-     cod2 += (matrixUI[u][i]-pu)*(matrixUI[u][i]-pu);
-   }
-   cod1 = sqrt(cod1);
-   cod2 = sqrt(cod2);
-   return con/(cod1+cod2);
- }
+double corr(int a, int u) {
+  double pa,pu;
+  pa = pu = 0.0;
+  for (int i = 0; i < movies; i++) {
+    pa += matrixUI[a][i];
+    pu += matrixUI[u][i];
+  }
+  pa /= movies;
+  pu /= movies;
+  double con=0;
+  for (int i = 0; i < movies; ++i)
+    con += (matrixUI[a][i]-pa)*(matrixUI[u][i]-pu);
+
+  double cod1 = 0;
+  double cod2 = 0;
+  for (int i = 0; i < movies; ++i) {
+    cod1 += (matrixUI[a][i]-pa)*(matrixUI[a][i]-pa);
+    cod2 += (matrixUI[u][i]-pu)*(matrixUI[u][i]-pu);
+  }
+  cod1 = sqrt(cod1);
+  cod2 = sqrt(cod2);
+  return con/(cod1+cod2);
+}
